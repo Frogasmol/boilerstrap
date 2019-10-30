fcd9dea27e59343aeec812e883a0247399afb847
Frogasmol <mathieutremblay.web@gmail.com>
2019-10-30 13:27:45 -0400

    essai de push

---
+++ src/js/app.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/**
+ *  Ajout de Bootstrap
+**/
+import "bootstrap";
+
+import "../css/style.scss";
+
+
+/* --------------- CODE --------------- */
+// console.log("--------------- JS ---------------");
+
+import { monCompteur, incrementeMonCompteur } from "./librairie";
+
+console.log(monCompteur);
+
+incrementeMonCompteur();
+
+console.log(monCompteur);
+
